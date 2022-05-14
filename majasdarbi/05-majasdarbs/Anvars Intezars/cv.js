b6b903447361de90964a05c7f347ce57be4831cf
Anvar331 <anvar.intezar@gmail.com>
2022-05-14 13:44:37 +0300

    SWITCH, JSObects

---
+++ majasdarbi/05-majasdarbs/Anvars Intezars/cv.js
@@ -0,0 +1,46 @@
+const NameSurname = {
+    name:"Anvars",
+    surname:"Intezars", 
+
+    get getName() {
+        return this.name;
+    },
+    get surname() {
+        return this.surname
+    }
+}
+
+let aboutMe = {
+    telephone:297754525,
+    mail:"anvar.intezar@gmail.com",
+    linkedin:"https://www.linkedin.com/in/anvarintezar",
+    github:"https://github.com/Anvar331",
+    location:"Riga, Latvia"
+}
+
+let workExperience = [
+    "Baltic Development Forum Summit", 
+    "Riga City Cuncil", 
+    "SIA Venitek",
+    "AS Versobank",
+    "SIA Universe Corporation",
+    "AS BlueOrange Bank",
+    "Self-Employed",
+    "Skandinavska Enskilda Banken AB"
+]
+
+let education = [
+    "SIA Progemeistars",
+    "International House",
+    "University of Latvia",
+    "Institut Français de Lettonie",
+    "SIA Baltic Compliance",
+    "Confucius Institute in Riga, Latvia",
+    "SIA ITGuru Sowftware Development"
+]
+
+let nonHumanLanguages = ["Java", "SQL", "HTML", "CSS", "JavaScript"]
+
+let languages = ["Latvian", "Russian", "English", "French", "Chinese"]
+
+let hobbies = ["Foreign languages", "books", "fitness", "progamming", "music"]
