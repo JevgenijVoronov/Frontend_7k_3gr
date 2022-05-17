151430a88ae09184e6154f8345d5d821488b1a49
Ivars_Jermanis <ivars.jermanis@gmail.com>
2022-05-17 21:53:56 +0300

    JS added

---
+++ majasdarbi/05-majasdarbs/IvarsJermanis/cv.js
@@ -0,0 +1,72 @@
+const NameSurname = {
+    name:"Ivars",
+    surname:"Jermanis",
+
+    get name() {
+        return this.name;
+    },
+    get surname() {
+        return this.surname
+    }
+}
+
+let aboutMe = {
+    mail:"ivars.jermanis@gmail.com",
+    linkedin:"https://www.linkedin.com/in/ivars-jermanis-9b56ba1b5/",
+
+    get mail() {
+        return this.mail
+    },
+    get linkedin() {
+        return this.linkedin
+    },
+}
+
+let workExperience = [
+    "DynaTech", 
+    "Discovercars Hire", 
+    "SIA RAUDA",
+    "KEPAK KIRKHAM",
+    "WEBHELP LATVIJA",
+    "INTERNATIONAL TRAVEL NETWORK",
+    "AMBERCRO",
+]
+
+let education = [
+    "Capstone: Retrieving, Processing, and Visualizing Data with Python",
+    "Using Databases with Python",
+    "Python Data Structures",
+    "Programming for Everybody (Getting Started with Python)",
+    "Coursera Python for Everybody Specialization",
+    "DSD DEUTSCHES SPRACHDIPLOM",
+    "University of Latvia"
+]
+
+let skills = [
+    "Attention to detail",
+    "Good organizational skills",
+    "Team player",
+    "Management skills",
+    "Growth mindset",
+    "Interpersonal skills",
+    "Flexibility and adaptability",
+    "Creative problem solving"
+]
+
+let FAQ = [
+    "Guitar and music",
+    "AI development",
+    "Languages",
+    "Sports and active lifestyle"
+]
+
+let languages = [
+    "Latvian",
+    "English",
+    "German",
+    "Russian",
+    "French",
+    "Spanish",
+    "Japanese"
+]
+
