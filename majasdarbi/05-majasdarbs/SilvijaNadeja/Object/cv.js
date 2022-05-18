58b4c6c1599c66b05856a4f601066d5b4c8e5faa
NadejSil <silvija.brante.rtu@gmail.com>
2022-05-18 16:23:38 +0300

    md5

---
+++ majasdarbi/05-majasdarbs/SilvijaNadeja/Object/cv.js
@@ -0,0 +1,54 @@
+const VardsUzvards = {
+    name:"Silvija",
+    surname:"Nadeja",
+
+    get name() {
+        return this.name;
+    },
+    get surname() {
+        return this.surname
+    }
+}
+
+let aboutMe = {
+    phone: "+371 28 773 492",
+    email: "silvija.nadeja@gmail.com",
+    linkedin: "https://www.linkedin.com/in/silvija-nadeja-20bba5146?originalSubdomain=lv",
+
+
+    get phone() {
+        return this.phone;
+    },
+    get email() {
+        return this.email;
+    },
+    get linkedin() {
+        return this.linkedin
+    }
+}
+
+let workExperience = [
+     "Secretary, Riga Technical university"
+]
+
+
+let education = [
+    "University of Latvia",
+    "Riga Building College",
+    "Riga Culture Secoundary school"
+    
+]
+
+let skills = [
+    "MS Office: Word, Excel, PowerPoint",
+    "LaTeX (basic)",
+    "HTML and CSS (basic)",
+
+]
+
+let languages = [
+    "Latvian",
+    "English",
+    "Russian"
+]
+
