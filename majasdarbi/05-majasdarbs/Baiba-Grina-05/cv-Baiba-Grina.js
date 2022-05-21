ba3c29d46848c2f7c61cf1534415dc80d5edf963
Baiba-Grina <grina.baiba@gmail.com>
2022-05-22 01:11:50 +0300

    05-majasdarbs

---
+++ majasdarbi/05-majasdarbs/Baiba-Grina-05/cv-Baiba-Grina.js
@@ -0,0 +1,37 @@
+const VardsUzvards = {
+    VardsUzvards: "Baiba",
+    Uzvards: "Grīna",
+    aboutMe: "Graphic Designer",
+
+    workExperience: [ 
+    "Freelance Graphic Designer 2016-now", 
+    "Part time Graphic Designer at Drukmeistars 2016-2017", 
+    "Junior Art Director at Creative Agency Adell 2016", 
+    "Graphic designer/Project manager/Print Operator at SIA Colorart Media 2012-2016" 
+    ],
+
+    Education: [ 
+        "2022 LU VUMC: Front-End Web Development Course", 
+        "2015-2016 Latvian Art Directors Club Ad School", 
+        "2013-2015 School of WEB Technology: Multimedia", 
+        "2009-2012 University of Latvia Bachelor’s Degree In Communication Science" 
+        ],
+}
+
+let skills = [
+    "Graphic Design", 
+    "Adobe Illustrator", 
+    "Adobe Indesign",
+    "Adobe Photoshop",
+    "Outdoor / Print",
+    "Advertising",
+    "Project Managing",
+    "Digital Printing",
+    "Car Wrapping Solutions"
+]
+
+let Language = [
+    "Latvian (Natvie)", 
+    "English (Professional Working Proficiency)", 
+    "Russian (Limited working proficiency)"
+]
