aa3e710427349085b9db12169ad69b83891a1617
Rasa Gravite <gravite.rasa@gmail.com>
2022-05-24 22:34:04 +0300

    5. majas darbs

---
+++ majasdarbi/05-majasdarbs/Rasa Grāvīte/if-else.js
@@ -0,0 +1,46 @@
+if (darbaDiena === 'Pirmdiena') {
+    console.log('1');
+} else if (darbaDiena === 'Otrdiena') {
+    console.log('2');
+} else if (darbaDiena === 'Trešdiena') {
+    console.log('3');
+} else if (darbaDiena === 'Ceturtdiena') {
+    console.log('4');
+} else if (darbaDiena === 'Piektdiena') {
+    console.log('5');
+} else if (darbaDiena === 'Sestdiena') {
+    console.log('6');
+} else if (darbaDiena === 'Svētdiena') {
+    console.log('7');
+} else {
+    console.log('Error');
+}
+
+// SWITCH //
+
+switch (darbaDiena) {
+ case "Pirmdiena":
+     console.log("1");
+    break;
+case "otrdiena":
+    console.log("2");
+    break;
+case "trešdiena":
+    console.log("3");
+    break;
+case "ceturtdiena":
+    console.log("4");
+    break;
+case "piektdien":
+    console.log("5");
+    break;
+case "sestdiena":
+    console.log("6");
+    break;
+case "svētdiena":
+    console.log("7");
+    break;
+default:
+    console.log("error")
+}
+
