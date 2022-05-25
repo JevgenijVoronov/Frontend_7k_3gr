b26c22c805ea8c4c927d52ba97a8819ac944239c
NadejSil <silvija.brante.rtu@gmail.com>
2022-05-25 19:46:15 +0300

    md6

--- majasdarbi/06-majasdarbs/SilvijaNadeja/script.js
+++ majasdarbi/06-majasdarbs/SilvijaNadeja/script.js
@@ -9,17 +9,17 @@
         Ja ievaditais skaitlis atbilst burtu masivam tad šis burts tiek izvadits uz ekranu 
         Spiežot pogas uz tastaturas aplikacija parbauda ievadito vertibu ar izveleto burtu un pažiņo par rezultatu
 */
-
+ 
 
 // Implementacija
 // 1) izvejdot massivu ar burtiem characters[]
-const characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 // 2) izvejtot mainigo "arrayId" šis mainigas saņems lietotaja ievadito skaitli
-let arrayId = 0;
+let arrayId = "";
 
 // 3) izvejtot mainigo "output" šis mainigas satur DOM elementu kur tiek izvadits izveletais burts (getElementById) 
-let output = document.getElementById("main");
+const output = document.getElementById("main");
 
 /* 4) izvejtot funkciju getValue() funkcijas ipašibas 
         neko nesaņem
@@ -27,13 +27,14 @@ let output = document.getElementById("main");
         izveidot parbaudi if else un parliecinaties ka ievietotais skatlis pieder intervalam no 0 lidz characters.lenght
         ja gadijuma izsaukt finkciju print value (5 punkts)
         ne gadijuma izvadit kļudas paziņojomu pec tam izsaukt funkciju getValue velreizi
-*/
+*/      
 const getValue = () => {
         arrayId = prompt("Ievadiet skaitli no 0 līdz 25");
         if (arrayId >= 0 && arrayId < characters.length) {
                 printValues();
         } else {
-                getValue()
+                alert(`Skaitlis ${arrayId} neatbilst`);
+                getValue();
         };
 
 };
@@ -43,22 +44,22 @@ const getValue = () => {
         izmaiņam defineto output mainigo iekšejo text (innerText) ar izveleto burtu masiva (characters[arrayId])
 */
 const printValues = () => {
-        output.innerHTML = characters[arrayId];
-}
+       output.innerText = characters[arrayId];
+};
 
 // 6) izsaucam funkciju getValue()
 getValue();
 /* 7) japievino event listener keyPress un parbaudit ievadito to tastaturas vertibu ar izveleto vertibu pirm tam
         ja gadijuma izvadit paziņojumu alert('PAREIZI') un izsaukt funkciju getValue() vel reizi
         ne gadijum izvadit paziņojumu alert('NEPAREIZI')
-*/
+*/      
 document.addEventListener("keypress", checkIt);
 
-function checkIt(keyPresed) {
-        if (keyPresed.key == characters[arrayId]) {
-                alert("PAREIZI");
+function checkIt(keyPressed) {
+        if (keyPressed.key == characters[arrayId]) {
+                alert("Pareizi");
                 getValue();
         } else {
-                alert("NEPAREIZI");
+                alert("Nepareizi");
         }
 };
