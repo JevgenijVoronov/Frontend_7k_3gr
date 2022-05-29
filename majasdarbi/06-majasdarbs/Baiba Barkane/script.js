00aa71d6f008ee30e1c3601105436ca2ec9864a7
BaibaBarkane <baiba.barkane@gmail.com>
2022-05-29 18:31:27 +0300

    6md

--- majasdarbi/06-majasdarbs/Baiba Barkane/script.js
+++ majasdarbi/06-majasdarbs/Baiba Barkane/script.js
@@ -14,7 +14,34 @@
 // Implementacija
 // 1) izvejdot massivu ar burtiem characters[]
 
-const characters = ["z", "a", "l", "k", "t", "i", "s"];
+const characters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
 // 2) izvejtot mainigo "arrayId" šis mainigas saņems lietotaja ievadito skaitli
 
