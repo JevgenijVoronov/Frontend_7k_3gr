9d5265c2330d4eaf2195b17c5286cd63c93b7a1b
EdgarsDa <edgarsdaina@gmail.com>
2022-05-29 11:15:15 +0300

    5md

--- majasdarbi/05-majasdarbs/Edgars Daina/Object/cv.js
+++ majasdarbi/05-majasdarbs/Edgars Daina/Object/cv.js
@@ -1,3 +1,33 @@
 const VardsUzvards = {
-    
+    vard: 'Edgars',
+    uzvards: 'Daina',
+    telefons: '+37125886438',
+    epasts: 'edgarsdaina@gmail.com',
+
+    darbaPieredze: [
+        '01.05.2022. - tagad - Ražošanas nodaļas vadītājs (AS "SAF tehnika")',
+        '01.05.2020. - 01.05.2022. - Radioaparatūras regulētājs (AS "SAF tehnika")',
+        '01.11.2016. - 01.05.2020. - Radioaparatūras montētājs (AS "SAF tehnika")',
+        '01.06.2011. - 2013. - Palīgstrādnieks (VSIA "Strenču PNS"(vasaras darbs))',
+    ],
+
+
+    izglitiba: [
+        '01.09.2014. - 01.02.2017. - Elektronikas speciālists (PIKC "Rīgas Tehniskā koledža")',
+        '01.09.2002. - 14.06.2014. - Vidējā vispārējā izglītība (Strenču novada vidusskola)'
+    ],
+
+    prasmes: [
+        'Labas iemaņas darbā ar datoru',
+        'Ražošanas darbu organizēšana',
+        'Gatavo produktu kvalitātes kontrole/testēšana/remonts',
+        'B kategorijas autovadītāja apliecība'
+    ],
+
+    valodas: [
+        'Angļu valoda - B2',
+        'Latviešu valoda - dzimtā',
+    ]
+
+
 }
