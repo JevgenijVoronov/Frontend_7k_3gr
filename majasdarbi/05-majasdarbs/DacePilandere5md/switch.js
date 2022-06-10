9ecdda1a53a0d346d1be16e420d7c942e60d2648
Dace Pilandere <cielavina.saul@inbox.lv>
2022-06-10 19:54:38 +0300

    papildinats 4.uzd.ar bildi un ikonu

---
+++ majasdarbi/05-majasdarbs/DacePilandere5md/switch.js
@@ -0,0 +1,25 @@
+switch (darbaDiena) {
+    case "Pirmdiena":
+        console.log("1");
+       break;
+   case "otrdiena":
+       console.log("2");
+       break;
+   case "trešdiena":
+       console.log("3");
+       break;
+   case "ceturtdiena":
+       console.log("4");
+       break;
+   case "piektdien":
+       console.log("5");
+       break;
+   case "sestdiena":
+       console.log("6");
+       break;
+   case "svētdiena":
+       console.log("7");
+       break;
+   default:
+       console.log("error")
+   }
