6d8820a3fc5601c742174e1d953ae3783c0fa457
Anvar331 <anvar.intezar@gmail.com>
2022-06-14 21:55:18 +0300

    dog application

--- majasdarbi/09-majasdarbs/forStudents/app.js
+++ majasdarbi/09-majasdarbs/forStudents/app.js
@@ -1,44 +1,25 @@
-/* 
-    Šajā majasdarba mes izveidosim nelielu aplikāciju, kura izvadīs uz ekrāna izvēlēto suņa šķirnes bildi.
-    Apkskatiet DogApiDemo.mp4 
+async function start() {
+    const response = await fetch("https://dog.ceo/api/breeds/list/all")
+    // waiting for promise to provide us our results
+    const data = await response.json()
+    createDogBreedList(data.message)
+}
 
-    Izmantosim API https://dog.ceo/dog-api/
+function createDogBreedList(breedList) {
+    document.getElementById("breed").innerHTML = `
+        <select> onchange="loadByBreed"(this.value)">
+             <option>Choose a desired dog breed</option>
+             ${Object.keys(breedList).map(function (breed) {
+                 return `<option> ${breed} </option>`
+        }).join('')}
+        </select>
+    `
+}
 
-    1)  Apskatiet failu data.js tur piejami visi dati par suņu šķirnēm.
-        Mums ir piejajams <select id='dogSelector'> kurā nepiecišams pievienot vairākus <option value="value">name</option>
-        Mēs varētu katru <option> elementu ierakstīt HTML, bet labak izmantot ciklu kurš izies cauri visiem datiem un izveidos mums nepiecišamos elementus
-
-        Implementācija: 
-        - Izvejdosim mainīgo dogSelector kurā saglabāsim mūsu <select> element var izmantot elementa id 'dogSelector' un getElementById() metodi
-        - izveidosim funkciju renderDogData(data) kura saņems suņa šķirnes datus 
-        - izmantojot cilku forEach() izesiem cauri datiem un pievienosim mūsu dogSelector elementam ar innerHTML nepiecišamo html:
-            `<option value=${breed.value}>${breed.name}</option>`
-        - izsaucam renderDogData() funkciju un rezultātā iegūsim visas iespejamāss suņu šķirnes iekš <select> elementa kā <options>.
-
-
-
-
-    2)  Izmantojot addEventListener('change', getDogImg) notikumu priekš dogSelector elementa, piesaistam šim notikumam funkciju kura kā parametru saņemt suņa attēlu.
-
-        Implementācija:
-        - pievienojam addEventListener pie dogSelector uz un piesaistām tam funkciju getDogImg
-        - pievinojam funkciju getDogImg
-        - fukcijā izveidojam string tipa mainīgo, kas saturēs API url priekš konkrētās suņa šķirnes. Šajā string vērtībā dinamiski jāievieto suņa šķirnes nosaukums no <select> elementa value attiecīgajā URL daļā. 
-            https://dog.ceo/api/breed/SUŅA-ŠKIRNE/images/random
-        - iegūstam nepieciešamo suņa šķirnes nosaukumu, izmantojot elementa dogSelector.value
-          tālak izmantojot fetch() metodi izvejdojam request priekš servera
-            fetch(url)
-                .then(response => response.json())
-                .then(data => renderDogData(data))
-                .catch(error => alert(error));
-
-
-
-    3)  Izvadit suņa bildi 
-        
-        Implementācija:
-        - izvedojam funkciju renderDogData() kurai kā parametru padosim mūsu data objektu 
-        - ivejdosim vēl vienu selektoru lai dabūt mūsu dogImg elementu 
-        - izmantojot innerHTML pievienot img tag ar src=${data.message}
-*/
+async function loadByBreed(breed) {
+    if (breed !="Choose a desired dog breed") {
+        const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
+        const data = await response.json()
+    }
+}
 
