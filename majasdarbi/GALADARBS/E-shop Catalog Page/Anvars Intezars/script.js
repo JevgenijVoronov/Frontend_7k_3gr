1e4160f70fcba45d882ad3925ae83a67b6be0142
Anvar331 <anvar.intezar@gmail.com>
2022-06-26 17:15:42 +0300

    JS, CSS, HTML amended

--- majasdarbi/GALADARBS/E-shop Catalog Page/Anvars Intezars/script.js
+++ majasdarbi/GALADARBS/E-shop Catalog Page/Anvars Intezars/script.js
@@ -22,3 +22,20 @@ function findProducts() {
 .then(console.log);
 }
 
+let buttonFindProducts = document.getElementById("findProducts");
+buttonFindProducts.addEventListener("click", function () {
+   findProducts();
+});
+
+
+function getProductsByCategory () {
+   fetch('https://dummyjson.com/products')
+.then(res => res.json())
+.then(console.log);
+}
+
+let buttonGetProductsByCategory = document.getElementById("productsByCategory");
+buttonGetProductsByCategory.addEventListener("click", function () {
+   getProductsByCategory();
+});
+
