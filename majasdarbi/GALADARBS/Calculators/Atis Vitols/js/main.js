ea342b5ed86a312ab96b553df92d1c909bcdc04a
npwillow <50518017+npwillow@users.noreply.github.com>
2022-06-27 17:49:44 +0300

    sākts

--- majasdarbi/GALADARBS/Calculators/Atis Vitols/js/main.js
+++ majasdarbi/GALADARBS/Calculators/Atis Vitols/js/main.js
@@ -17,4 +17,53 @@
 // number btn click function:
 
 // keyboard event enter:
+// let keepScoreString = "";
+
+// const updateDisp = (input) => {
+//     let output = document.getElementById("outputs")
+
+//     if (input == "C") {
+//         output.innerHTML = ""
+//         keepScoreString = "";
+//     } else if (input == "=") {
+//         console.log(Number(keepScoreString))
+//     } else{
+//         output.innerHTML += input;
+//         keepScoreString += input;
+//     }
+// };
+
+// const number = (number) => {
+//     console.log(number.path[0].innerHTML)
+//     updateDisp(number.path[0].innerHTML)
+// } 
+
+// const btn = document.getElementsByClassName("btn");
+// for (let pressed of btn) [
+//     pressed.addEventListener("click", number)
+// ]
+
+// document.addEventListener("keydown", (button) => {
+//     var name = button.key;
+//     console.log(name)
+// })
+
+
+
+// function test(btn) {
+//     console.log(btn.path[0].innerHTML)
+// }
+
+
+const numberButtons = document.querySelectorAll('[number]')
+for (let pressed of numberButtons) [
+    pressed.addEventListener("click", function test() {console.log("numberButtons")})
+]
+
+// const operationButtons = document.querySelectorAll('[data-operation]')
+// const equalsButton = document.querySelector('[data-equals]')
+// const deleteButton = document.querySelector('[data-delete]')
+// const allClearButton = document.querySelector('[data-all-clear]')
+// const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+// const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
