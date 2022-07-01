72fd9843338ea2cadb0897e16c3920863eb7f3d6
Vitaly Kirey <99085043+vitalykirey@users.noreply.github.com>
2022-07-01 11:31:35 +0300

    Galadarbs -Emarket

---
+++ majasdarbi/GALADARBS/E-shop Catalog Page/Vitalijs Kirejs/JS/data.js
@@ -0,0 +1,88 @@
+const data = [
+		{
+			"id": 			"pizza-1",
+			"name": 		"Margarita Pizza",
+			"price": 		"10",
+			"description": 	"Pizza sauce, oregano, cheese, tomato",
+			"image": 		"https://thumb.tildacdn.com/tild3630-3739-4630-b135-636564326335/-/resize/275x/-/format/webp/Margarita-removebg-p.png",
+			"category": 	"pizza",
+			"ingredients":	"Pizza sauce, oregano"
+		},
+	
+		{
+			"id": 			"pizza-2",
+			"name": 		"Mushrooms Pizza",
+			"price": 		"6,99",
+			"description": 	"Pizza sauce, cheese, mushrooms",
+			"image": 		"https://thumb.tildacdn.com/tild3564-6533-4635-b461-656362303536/-/resize/275x/-/format/webp/senu_pica-removebg-p.png",
+			"category": 	"pizza",
+			"ingredients":	"Pizza sauce"
+		},
+	
+		{
+			"id": 			"pizza-3",
+			"name": 		"Salami pizza",
+			"price": 		"7,99",
+			"description": 	"Pizza sauce, cheese, salami",
+			"image": 		"https://thumb.tildacdn.com/tild3663-6334-4561-a135-373236643866/-/resize/275x/-/format/webp/Salami-removebg-prev.png",
+			"category": 	"pizza",
+			"ingredients": 	"Pizza sauce"
+		},
+	
+	
+		{
+			"id": 			"pizza-4",
+			"name": 		"Kebab Pizza",
+			"price": 		"9,99",
+			"description": 	"Pizza sauce, Tzatziki-souce, chicken kebab meat, onion",
+			"image": 		"https://pizza.az/upload/resize_cache/iblock/3fd/260_260_040cd750bba9870f18aada2478b24840a/3fd2ddc518e442ca561c193c2393878c.jpg",
+			"category": 	"pizza",
+			"ingredients":	"Pizza sauce, Tzatziki-souce"
+		},
+	
+		{
+			"id": 			"drink-1",
+			"name": 		"Pepsi",
+			"price": 		"3,50",
+			"description": 	"Pepsi",
+			"image": 		"https://static.tildacdn.com/tild3462-6262-4839-b536-326434643236/pepsi-15.png",
+			"category": 	"drinks"
+		},
+	
+		{
+			"id": 			"drink-2",
+			"name": 		"Mirinda",
+			"price": 		"3,50",
+			"description": 	"Mirinda",
+			"image": 		"https://static.tildacdn.com/tild3362-6534-4033-b861-366330303864/mirinda-15.png",
+			"category": 	"drinks"
+		},
+	
+		{
+			"id": 			"drink-3",
+			"name": 		"Pepsi",
+			"price": 		"1,99",
+			"description": 	"Pepsi",
+			"image": 		"https://static.tildacdn.com/tild6337-3138-4265-b066-313238353835/pepsi-033.png",
+			"category": 	"drinks"
+		},
+	
+		{
+			"id": 			"drink-4",
+			"name": 		"Coca-Cola",
+			"price": 		"1,99",
+			"description": 	"Coca-Cola",
+			"image": 		"https://sushiking.ee/uploads/items/201/main.jpg",
+			"category": 	"drinks"
+		},
+	
+		{
+			"id": 			"drink-5",
+			"name": 		"Coca-Cola",
+			"price": 		"3,50",
+			"description": 	"Coca-Cola",
+			"image": 		"https://main-cdn.sbermegamarket.ru/mid9/hlr-system/18/54/01/35/14/62/3/100023688332b0.jpg",
+			"category": 	"drinks"
+		}
+	
+]
