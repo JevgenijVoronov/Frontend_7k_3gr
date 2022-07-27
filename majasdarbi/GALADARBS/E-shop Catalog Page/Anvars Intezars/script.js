9de75d5fc2ac856d5d212b495ed6623b332be5d7
Anvar331 <anvar.intezar@gmail.com>
2022-07-27 21:21:52 +0300

    JS amended

--- majasdarbi/GALADARBS/E-shop Catalog Page/Anvars Intezars/script.js
+++ majasdarbi/GALADARBS/E-shop Catalog Page/Anvars Intezars/script.js
@@ -1,6 +1,15 @@
 
 let inputElementSelector = document.getElementById("searchProduct");
+var mainContainer = document.getElementById("allProductData")
 
+function appendData(data) {
+   var mainContainer = document.getElementById("allProductData");
+   for (var i = 0; i < data.length; i++) {
+     var div = document.createElement("div");
+     div.innerHTML = 'product: ' + data[i].brand + ' ' + data[i].category;
+     mainContainer.appendChild(div);
+   }
+ }
 
 function getAllProducts() {
    fetch('https://dummyjson.com/products')
