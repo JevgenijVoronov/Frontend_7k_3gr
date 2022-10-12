ffd1f790a82a7500de774337a6c782f03d3a72db
Jevgenij Voronov <jevgenijsvoronovs@gmail.com>
2022-10-12 21:25:21 +0300

    zekaAdjust

--- majasdarbi/GALADARBS/E-shop Catalog Page/Anvars Intezars/script.js
+++ majasdarbi/GALADARBS/E-shop Catalog Page/Anvars Intezars/script.js
@@ -1,8 +1,11 @@
 
 let inputElementSelector = document.getElementById("searchProduct");
+let buttonGetAllProducts = document.getElementById("getAllProducts");
+let buttonGetProductsByCategory = document.getElementById("productsByCategory");
+let renderProduct = document.getElementById("render-products");
 const mainUL = document.createElement('ul')
 
-mainUL.appendChild(productLI);
+// mainUL.appendChild(productLI);
 
 
 /*This is old code, which for now I do not want to remove
@@ -21,14 +24,16 @@ function getAllProducts() {
 
 //Testing new method in order to create list of products in HTML file
 function createList(data) {
-   const mainUL = document.createElement('ol');
-   for (let i = 0; i < data.result.length; i++) {
-     const productLI = document.createElement('li');
-     productLI.innerHTML = data.result[i].title;
+   console.log(data);
+
+   for (let i = 0; i < data.products.length; i++) {
+      renderProduct.innerHTML += `<li>${data.products[i].title}</li>`
+   //   const productLI = document.createElement('li');
+   //   productLI.innerHTML = data.products[i].title;
+   }
+}
 
 
-
-let buttonGetAllProducts = document.getElementById("getAllProducts");
 buttonGetAllProducts.addEventListener("click", function () {
    getAllProducts();
 });
@@ -36,8 +41,8 @@ buttonGetAllProducts.addEventListener("click", function () {
 
 function findProducts() {
    fetch(`https://dummyjson.com/products/search?q=${inputElementSelector.value}`)
-.then(res => res.json())
-.then(console.log);
+   .then(res => res.json())
+   .then(data => createList(data));
 }
 
 let buttonFindProducts = document.getElementById("findProducts");
@@ -48,11 +53,10 @@ buttonFindProducts.addEventListener("click", function () {
 
 function getProductCategory () {
    fetch('https://dummyjson.com/products/categories')
-.then(res => res.json())
-.then(console.log);
+   .then(res => res.json())
+   .then(console.log);
 }
 
-let buttonGetProductsByCategory = document.getElementById("productsByCategory");
-buttonGetProductsByCategory.addEventListener("click", function () {
-   getProductsByCategory();
-});
+
+
+buttonGetProductsByCategory.addEventListener("click", getProductCategory);
